627ca7d681558fcf19a062ebc205875a2974b6b5
Dinesh Thakare <dineshthakare318@gmail.com>
2022-10-04 15:48:04 +0530

    firstjs

---
+++ file.js
@@ -0,0 +1,51 @@
+var op1="arithmetic operator";
+console.log(op1);
+let a=50;
+let b=30;
+let c=a+b;
+let c1=a-b;
+let c2=a/b;
+let c3=a*b;
+let c4=a%b;
+console.log(c,c1,c2,c3,c4);
+var op2="assignment operator";
+console.log(op2);
+let c5=a+=b;
+let c6=a-=b;
+let c7=a*=b;
+let c8=a/=b;
+console.log(c5,c6,c7,c8);
+
+ var op3="Relational operator"; 
+ console.log(op3);
+var x=45;
+var z=20;
+var r1=x==z;
+var r2=x!=z;
+var r3=x>z;
+var r4=x<z;
+console.log(r1,r2,r3,r4);
+
+var op4="logical operator";
+console.log(op4);
+var  d=5;
+var e=4;
+var a1=7;
+var a2=5;
+var s1=d>e && a1>a2;
+var s2=d>e || a1<a2;
+console.log(s1,s2);
+
+var op5= "Incre decrement operator";
+console.log(op5);
+var a3=15;
+var a4=11;
+var g=a3++;
+var j=a4--;
+console.log(g,j);
+
+var op6="conditional operator";
+console.log(op6);
+var r5=(a>b)? a:b;
+console.log(r5);
+
